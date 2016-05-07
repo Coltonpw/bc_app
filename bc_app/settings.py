d57514bc970904f6df1d524c0e13c4aef7cef02d
rpcrimi <rpcrimi@gmail.com>
2016-05-06 22:47:54 -0600

    second commit

--- bc_app/settings.py
+++ bc_app/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'bc_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'db_mongo',
     }
 }
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'MST'
 
 USE_I18N = True
 
--- polls/models.py
+++ polls/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from djangotoolbox.fields import ListField
+
+class Question(models.Model):
+	text = models.TextField()
+	tags = ListField()
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField()
+	votes = models.IntegerField(default=0)
